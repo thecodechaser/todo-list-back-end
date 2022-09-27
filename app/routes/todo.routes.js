210175afbab41d55b86a6b8d6b0bb06ca632dc50
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 02:47:06 +0530

    Create route for create a todo

--- app/routes/todo.routes.js
+++ app/routes/todo.routes.js
@@ -1,7 +1,10 @@
 module.exports = app => {
-  const tutorials = require("../controllers/tutorial.controller.js");
+  const todos = require("../controllers/todo.controller.js");
 
   var router = require("express").Router();
+
+   // Create a new Tutorial
+   router.post("/", todos.create);
 
  
 };
