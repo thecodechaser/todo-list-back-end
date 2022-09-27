4607bf0742930f25f84251cd71c49258e9831e8e
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 01:02:57 +0530

    Setup port for the server

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
