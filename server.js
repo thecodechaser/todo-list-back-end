4f36b47256ced4df338660affe52275e892ecd6a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 01:05:42 +0530

    Add config to listen port

--- server.js
+++ server.js
@@ -18,3 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to thecodechaser application." });
 });
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
